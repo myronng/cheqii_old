da8a4052f123b52a6877016f37f6b768c1628382
Myron <myronng5@gmail.com>
2021-07-16 15:00:30 -0600

    Improved legibility for light theme Allow upgrading anonymous to email login for existing emails

--- components/auth/TextField.tsx
+++ components/auth/TextField.tsx
@@ -8,24 +8,9 @@ export const TextField = styled(ValidateTextField)`
   }
 
   & .MuiInputBase-root.MuiInputBase-adornedStart {
-    ${theme.breakpoints.up("xs")} {
-      height: 48px;
-    }
-    ${theme.breakpoints.up("md")} {
-      height: 64px;
-    }
-
-
     & .MuiInputBase-input {
       border-bottom-left-radius: 0;
       border-top-left-radius: 0;
-
-      ${theme.breakpoints.up("xs")} {
-        padding: 12px ${theme.spacing(1)};
-      }
-      ${theme.breakpoints.up("md")} {
-        padding: 20px ${theme.spacing(1)};
-      }
     }
 
     & .MuiOutlinedInput-notchedOutline legend {
