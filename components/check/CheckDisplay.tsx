34fdc9770c74d0a216574d57b8d5b511d6b5744d
Myron <myronng5@gmail.com>
2021-08-16 23:24:14 -0600

    Remove unneccessary fragment

--- components/check/CheckDisplay.tsx
+++ components/check/CheckDisplay.tsx
@@ -41,99 +41,97 @@ export const CheckDisplay = styled((props: CheckDisplayProps) => {
   const localItemsIndex = props.items.length;
 
   return (
-    <>
-      <div className={`Grid-container ${props.className}`}>
-        <div className="Grid-row">
-          <span className="Grid-header">Item</span>
-          <span className="Grid-header Grid-numeric">Cost</span>
-          <span className="Grid-header">Buyer</span>
-          {props.contributors.map((contributor, contributorIndex) => (
-            <div className="Grid-cell Grid-numeric" key={contributorIndex}>
-              <Input
-                defaultValue={contributor}
-                id={`contributor-${contributorIndex}`}
-                onBlur={(e) => props.onContributorBlur(e, "existing", contributorIndex)}
-                required
-              />
-            </div>
-          ))}
-          {props.localContributors.map((localContributor, localContributorIndex) => (
-            <div className="Grid-cell Grid-numeric" key={localContributorIndex}>
-              <Input
-                defaultValue={localContributor}
-                id={`contributor-${localContributorIndex}`}
-                onBlur={(e) => props.onContributorBlur(e, "new", localContributorIndex)}
-                required
-              />
-            </div>
-          ))}
-        </div>
-        {allItems.map((item, itemIndex) => {
-          let transactionType: TransactionType;
-          let transactionIndex: number;
-          let rowClass: string;
-          if (itemIndex < localItemsIndex) {
-            rowClass = "Grid-row";
-            transactionIndex = itemIndex;
-            transactionType = "existing";
-          } else {
-            rowClass = "Grid-row Grid-new";
-            transactionIndex = itemIndex - localItemsIndex;
-            transactionType = "new";
-          }
-          return (
-            <div className={rowClass} key={item.id}>
-              <div className="Grid-cell">
-                <Input
-                  defaultValue={item.name}
-                  id={`name-${item.id}`}
-                  onBlur={(e) => props.onItemNameBlur(e, transactionType, transactionIndex)}
-                  required
-                />
-              </div>
-              <div className="Grid-cell Grid-numeric">
-                <Input
-                  defaultValue={item.cost}
-                  id={`cost-${item.id}`}
-                  inputMode="numeric"
-                  numberFormat="currency"
-                  onBlur={(e) => props.onCostBlur(e, transactionType, transactionIndex)}
-                  required
-                />
-              </div>
-              <div className="Grid-cell">
-                <Select
-                  defaultValue={item.buyer}
-                  id={`buyer-${item.id}`}
-                  onChange={(e) => {
-                    if (typeof props.onBuyerChange === "function") {
-                      props.onBuyerChange(e, transactionType, transactionIndex);
-                    }
-                  }}
-                  options={props.contributors}
-                  required
-                />
-              </div>
-              {item.split?.map((split, splitIndex) => (
-                <div className="Grid-cell Grid-numeric" key={splitIndex}>
-                  <Input
-                    defaultValue={split}
-                    id={`split-${item.id}-${splitIndex}`}
-                    inputMode="numeric"
-                    numberFormat="integer"
-                    onBlur={(e) =>
-                      props.onSplitBlur(e, transactionType, transactionIndex, splitIndex)
-                    }
-                    pattern="\p{N}*"
-                    required
-                  />
-                </div>
-              ))}
-            </div>
-          );
-        })}
+    <div className={`Grid-container ${props.className}`}>
+      <div className="Grid-row">
+        <span className="Grid-header">Item</span>
+        <span className="Grid-header Grid-numeric">Cost</span>
+        <span className="Grid-header">Buyer</span>
+        {props.contributors.map((contributor, contributorIndex) => (
+          <div className="Grid-cell Grid-numeric" key={contributorIndex}>
+            <Input
+              defaultValue={contributor}
+              id={`contributor-${contributorIndex}`}
+              onBlur={(e) => props.onContributorBlur(e, "existing", contributorIndex)}
+              required
+            />
+          </div>
+        ))}
+        {props.localContributors.map((localContributor, localContributorIndex) => (
+          <div className="Grid-cell Grid-numeric" key={localContributorIndex}>
+            <Input
+              defaultValue={localContributor}
+              id={`contributor-${localContributorIndex}`}
+              onBlur={(e) => props.onContributorBlur(e, "new", localContributorIndex)}
+              required
+            />
+          </div>
+        ))}
       </div>
-    </>
+      {allItems.map((item, itemIndex) => {
+        let transactionType: TransactionType;
+        let transactionIndex: number;
+        let rowClass: string;
+        if (itemIndex < localItemsIndex) {
+          rowClass = "Grid-row";
+          transactionIndex = itemIndex;
+          transactionType = "existing";
+        } else {
+          rowClass = "Grid-row Grid-new";
+          transactionIndex = itemIndex - localItemsIndex;
+          transactionType = "new";
+        }
+        return (
+          <div className={rowClass} key={item.id}>
+            <div className="Grid-cell">
+              <Input
+                defaultValue={item.name}
+                id={`name-${item.id}`}
+                onBlur={(e) => props.onItemNameBlur(e, transactionType, transactionIndex)}
+                required
+              />
+            </div>
+            <div className="Grid-cell Grid-numeric">
+              <Input
+                defaultValue={item.cost}
+                id={`cost-${item.id}`}
+                inputMode="numeric"
+                numberFormat="currency"
+                onBlur={(e) => props.onCostBlur(e, transactionType, transactionIndex)}
+                required
+              />
+            </div>
+            <div className="Grid-cell">
+              <Select
+                defaultValue={item.buyer}
+                id={`buyer-${item.id}`}
+                onChange={(e) => {
+                  if (typeof props.onBuyerChange === "function") {
+                    props.onBuyerChange(e, transactionType, transactionIndex);
+                  }
+                }}
+                options={props.contributors}
+                required
+              />
+            </div>
+            {item.split?.map((split, splitIndex) => (
+              <div className="Grid-cell Grid-numeric" key={splitIndex}>
+                <Input
+                  defaultValue={split}
+                  id={`split-${item.id}-${splitIndex}`}
+                  inputMode="numeric"
+                  numberFormat="integer"
+                  onBlur={(e) =>
+                    props.onSplitBlur(e, transactionType, transactionIndex, splitIndex)
+                  }
+                  pattern="\p{N}*"
+                  required
+                />
+              </div>
+            ))}
+          </div>
+        );
+      })}
+    </div>
   );
 })`
   ${({ contributors = [], theme }) => `
