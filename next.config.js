095e8ebbcbf8cf940fab2e15fdff55ab0aef89ae
Myron <myronng5@gmail.com>
2023-01-12 19:12:50 -0700

    Fix modularizeImports in next.config.js
    Signed-off-by: Myron <myronng5@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 const withPwa = require("next-pwa");
 
 const config = {
-  experimental: {
-    modularizeImports: {
-      "@mui/icons-material": {
-        transform: "@mui/icons-material/{{member}}",
-      },
-      "@mui/lab": {
-        transform: "@mui/lab/{{member}}",
-      },
-      "@mui/material": {
-        transform: "@mui/material/{{member}}",
-      },
-    },
-  },
   i18n: {
     locales: ["en-CA"],
     defaultLocale: "en-CA",
   },
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+  },
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+    "@mui/lab": {
+      transform: "@mui/lab/{{member}}",
+    },
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
   },
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
